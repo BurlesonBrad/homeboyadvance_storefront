74fad7030483843bd351dc308511183577e5526d
Alan Rodriguez <alanro@microsoft.com>
2016-01-25 19:46:36 -0800

    Initial commit

---
+++ footer.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package storefront
+ */
+?>
+
+		</div><!-- .col-full -->
+	</div><!-- #content -->
+
+	<?php do_action( 'storefront_before_footer' ); ?>
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="col-full">
+
+			<?php
+			/**
+			 * @hooked storefront_footer_widgets - 10
+			 * @hooked storefront_credit - 20
+			 */
+			do_action( 'storefront_footer' ); ?>
+
+		</div><!-- .col-full -->
+	</footer><!-- #colophon -->
+
+	<?php do_action( 'storefront_after_footer' ); ?>
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+<!-- GOOGLE ANALTYICS -->
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+  var pageTracker = _gat._getTracker("UA-63702459-1");
+  pageTracker._trackPageview();
+} catch(err) {}
+</script>
+
+</body>
+</html>
